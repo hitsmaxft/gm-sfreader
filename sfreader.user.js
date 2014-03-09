b4cd24ad08700935903f265cbb2406d3eb5fab06
hitsmaxft <mfthits@gmail.com>
2014-03-09 21:45:20 +0800

    fix load bug

--- sfreader.user.js
+++ sfreader.user.js
@@ -6,78 +6,92 @@
 // @include     http://comic.sfacg.com/*
 // @version     3.0
 // @require http://code.jquery.com/jquery-1.11.0.min.js
-
+//
 // @grant GM_info
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant unsafeWindow
-
-// @require http://sizzlemctwizzle.com/130400.js?show
+//
+// @require http://sizzlemctwizzle.com/130400js?show
 // ==/UserScript==
 
+var debug_mode = false;
+
+if (typeof unsafeWindow == "undefined") {
+	unsafeWindow = window
+
+}
+
+//port jquery into unsafeWindow
+if(debug_mode) {
+	unsafeWindow.$=jQuery
+	unsafeWindow.jQuery=jQuery
+}
+
 this.$ = this.jQuery = jQuery.noConflict(true)
-var debug_mode = false
 
-(function(jQuery, debug_mode){
-	var $ = jQuery
 
-	var debug = function (msg) {
-		if (debug_mode) {
-			console.log("debug: " + msg)
+;(
+	function(jQuery){
+		var $ = jQuery
+
+		var debug = function (msg) {
+			if (debug_mode) {
+				console.log(msg)
+			}
 		}
-	}
 
-	var SfReader = function() {
-		this.debug = debug_mode
+		var SfReader = function() {
+			this.debug = debug_mode
 
-		this.initAttr = function(){
-			var attr = [
-			'comicName'
-			, 'picCount'
-			, 'picAy'
-			]
+			this.initAttr = function(){
+				var attr = [
+				'comicName'
+				, 'picCount'
+				, 'picAy'
+				]
 
-			for (i in attr) {
-				var name = attr[i]
-				this[name] = unsafeWindow[name]
+				for (i in attr) {
+					var name = attr[i]
+					this[name] = unsafeWindow[name]
+				}
+
+				this.cur = unsafeWindow.GetArgsFromHref(unsafeWindow.location.href, "p") - 1
+				this.hostId = unsafeWindow.getHost()
+				this.host = unsafeWindow.hosts[unsafeWindow.getHost()]
+				this.cache = [];
+
+				this.$curPic = $("#curPic")
+
+				this.$statusBar = this.renderStatusBar()
+				this.notif = this.$statusBar.find("#reader_notification")
+				this.$pager_text = this.$statusBar.find("#page_indicator span")
+				this.notif_text = this.notif.find("span")
+
+				var imgLoadEvent = $.Deferred()
+				this.$EImgLoading = imgLoadEvent
+
+				$("#curPic").on('load', function(e) {
+					$(this).get(0).scrollIntoView()
+					imgLoadEvent.resolve(function(){})
+				})
+
+
+				$(document.body).prepend(this.$statusBar)
 			}
 
-			this.cur = unsafeWindow.GetArgsFromHref(unsafeWindow.location.href, "p") - 1
-			this.hostId = unsafeWindow.getHost()
-			this.host = unsafeWindow.hosts[unsafeWindow.getHost()]
-			this.cache = [];
-
-			this.$curPic = $("#curPic")
-
-			this.$statusBar = this.renderStatusBar()
-			this.notif = this.$statusBar.find("#reader_notification")
-			this.$pager_text = this.$statusBar.find("#page_indicator span")
-			this.notif_text = this.notif.find("span")
-
-			var imgLoadEvent = $.Deferred()
-			this.$EImgLoading = imgLoadEvent
-
-			$("#curPic").on('load', function(e) {
-				$(this).get(0).scrollIntoView()
-				imgLoadEvent.resolve(function(){})
-			})
-
-
-			$(document.body).prepend(this.$statusBar)
 		}
 
-	}
+		SfReader.prototype.hookRenderPager= function() {
+			setInterval((function() {
+				this.$pager_text.text("第" + (this.cur+1) + "/" + this.picCount + "页")
 
-	SfReader.prototype.hookRenderPager= function() {
-		setInterval((function() {
-			this.$pager_text.text("第" + (this.cur+1) + "/" + this.picCount + "页")
+			}).bind(this), 100);
+		}
 
-		}).bind(this), 100);
-	}
-
-	SfReader.prototype.renderStatusBar= function(msg) {
+		SfReader.prototype.renderStatusBar= function(msg) {
 			var box = $("<div></div>")
 			box.attr("id", "sfreader_box")
 			box.attr("style", "position:fixed;z-index:9999999;background-color:none;padding:10px 30px;width:100%")
@@ -96,14 +110,14 @@ var debug_mode = false
 			box.append(pager_box)
 			box.append(inner_box)
 			return  box
-	}
+		}
 
-	SfReader.prototype.sendMsgWithCallback = function(callback, msg, done_text) {
-		this.notif_text.text(msg)
-		this.notif.fadeIn()
-		this.$EImgLoading.done(
-			(function(callback){
-				debug("loading fadeout")
+		SfReader.prototype.sendMsgWithCallback = function(callback, msg, done_text) {
+			this.notif_text.text(msg)
+			this.notif.fadeIn()
+			this.$EImgLoading.done(
+				(function(callback){
+					debug("loading fadeout")
 				// fadeOut with done text
 				setTimeout( (function() {
 					this.notif.fadeOut()
@@ -111,78 +125,78 @@ var debug_mode = false
 						this.notif_text.text(done_text)
 				}).bind(this), 600)
 			}).bind(this)
-		)
+				)
 
-		callback()
-	}
-
-	SfReader.prototype.sendMsg = function(msg, timeout) {
-		var latency = timeout || 2;
-		this.notif_text.text(msg)
-		this.notif.fadeIn()
-		setTimeout(
-			(function(){this.fadeOut()}).bind(this.notif)
-		, 400)
-	}
-	SfReader.prototype.start = function(page) {
-		this.initAttr();
-
-		this.bindPagerButton();
-		this.hookRenderPager();
-		debug("enjoy sf reader")
-	}
-
-	SfReader.prototype.bindPagerButton = function() {
-
-		var pagers = $(".page_turning")
-		var pre_links  =  pagers.find(".redfont_input:contains(上一页)")
-
-		var next_links  = pagers.find(".redfont_input:contains(下一页)")
-
-		next_links.push($("#curPic").parent()[0])
-
-		var self = this
-
-		pre_links.each(function(ind , item){
-			item.href = "javascript:;"
-			item.onclick = (function(e) {
-				e.preventDefault();
-				this.nav2Page(-1);
-			}).bind(self)
-		})
-		next_links.each(function(ind , item){
-			item.href = "javascript:;"
-			item.onclick = (function(e) {
-				e.preventDefault();
-				this.nav2Page(1);
-			}).bind(self)
-		})
-
-	}
-
-	SfReader.prototype.setImg = function(page) {
-		var imgEl = this.$curPic
-		imgEl.attr("src" , this.host  + this.picAy[page])
-		this.cur = page
-		debug("set img url 2 " + imgEl.src)
-	}
-	SfReader.prototype.render = function(page) {
-		debug("rend nothing with page " + page)
-	}
-	SfReader.prototype.nav2Page = function(pos) {
-		if (pos !== -1  && pos !== 1) {
-			debug("erro in pos nav : " + pos)
-			return false
+			callback()
 		}
 
-		var page_id = parseInt(this.cur, 0) + parseInt(pos, 0)
+		SfReader.prototype.sendMsg = function(msg, timeout) {
+			var latency = timeout || 2;
+			this.notif_text.text(msg)
+			this.notif.fadeIn()
+			setTimeout(
+				(function(){this.fadeOut()}).bind(this.notif)
+				, 400)
+		}
+		SfReader.prototype.start = function(page) {
+			this.initAttr();
 
-		if (
-			page_id < 0
-			|| page_id >= this.picCount
-			) {
+			this.bindPagerButton();
+			this.hookRenderPager();
+			debug("enjoy sf reader")
+		}
 
-			debug("out of range")
+		SfReader.prototype.bindPagerButton = function() {
+
+			var pagers = $(".page_turning")
+			var pre_links  =  pagers.find(".redfont_input:contains(上一页)")
+
+			var next_links  = pagers.find(".redfont_input:contains(下一页)")
+
+			next_links.push($("#curPic").parent()[0])
+
+			var self = this
+
+			pre_links.each(function(ind , item){
+				item.href = "javascript:;"
+				item.onclick = (function(e) {
+					e.preventDefault();
+					this.nav2Page(-1);
+				}).bind(self)
+			})
+			next_links.each(function(ind , item){
+				item.href = "javascript:;"
+				item.onclick = (function(e) {
+					e.preventDefault();
+					this.nav2Page(1);
+				}).bind(self)
+			})
+
+		}
+
+		SfReader.prototype.setImg = function(page) {
+			var imgEl = this.$curPic
+			imgEl.attr("src" , this.host  + this.picAy[page])
+			this.cur = page
+			debug("set img url 2 " + imgEl.src)
+		}
+		SfReader.prototype.render = function(page) {
+			debug("rend nothing with page " + page)
+		}
+		SfReader.prototype.nav2Page = function(pos) {
+			if (pos !== -1  && pos !== 1) {
+				debug("erro in pos nav : " + pos)
+				return false
+			}
+
+			var page_id = parseInt(this.cur, 0) + parseInt(pos, 0)
+
+			if (
+				page_id < 0
+				|| page_id >= this.picCount
+				) {
+
+				debug("out of range")
 			this.sendMsg("到头啦, 看其他话吧")
 			return false
 		}
@@ -190,13 +204,13 @@ var debug_mode = false
 		debug(" go to pageid : " + page_id)
 
 		this.sendMsgWithCallback(
-				(function(){
-					debug("make resolve")
-					this.setImg(page_id)
-				}).bind(this)
-				, "翻页中..."
-				, "翻页完成"
-		)
+			(function(){
+				debug("make resolve")
+				this.setImg(page_id)
+			}).bind(this)
+			, "翻页中..."
+			, "翻页完成"
+			)
 
 		debug("end of callback")
 
@@ -206,10 +220,8 @@ var debug_mode = false
 	var reader  = new SfReader()
 	
 
-	$(document).ready(
-		function() {
+	$(document).ready(function() {
 			reader.start()
-		}
-	)
+	})
 
-})($, debug_mode)
+})($)
